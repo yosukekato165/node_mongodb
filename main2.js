aaaaf27b9c0044b3d7efc890f03844f92b118965
yosukekato165 <padyk165@gmail.com>
2021-12-08 23:38:45 +0900

    add log and change port

--- main2.js
+++ main2.js
@@ -1,6 +1,8 @@
 import express from "express";
 const app = express();
 
+const port = 3030;
+
 app.post("/", function (req, res) {
   const data = { name: "tomato" };
   data.name = req.body.name;
@@ -8,4 +10,6 @@ app.post("/", function (req, res) {
   res.json(data);
 });
 
-app.listen(3030);
+app.listen(port);
+
+console.log(`The server has started and is listening on port number: ${port}`);
