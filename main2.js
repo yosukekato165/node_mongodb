be32ce39325520aedb04606808088339deed9935
yosukekato165 <padyk165@gmail.com>
2021-12-17 22:24:47 +0900

    aaa

--- main2.js
+++ main2.js
@@ -9,6 +9,10 @@ app.post("/hello", (req, res) => {
   res.send("Hello " + req.body.name);
 });
 
+app.post("/hey", (req, res) => {
+  res.send("Hey" + req.body.name);
+});
+
 app.listen(port, () => {
   console.log("Application started");
 });
