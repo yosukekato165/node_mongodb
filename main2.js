f655c4a11df2a57e96fe6305f66e79ca6342b9a1
yosukekato165 <padyk165@gmail.com>
2021-12-18 17:14:36 +0900

    success to return data from mongodb

--- main2.js
+++ main2.js
@@ -1,9 +1,25 @@
 import express from "express";
+import dotenv from "dotenv";
+import { mongodb } from "./mongodb.js";
+
+dotenv.config();
+
+const REFERENCE = "/reference";
+const REGISTER = "/register";
 const app = express();
 
 const port = 3030;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 const hey = [
   { name: "Tom", age: "18" },
@@ -16,6 +32,13 @@ app.post("/hello", (req, res) => {
 
 app.post("/hey", (req, res) => {
   res.json(hey);
+});
+
+app.post(REFERENCE, async (req, res) => {
+  // console.log(JSON.parse(req.body));
+  let result = await mongodb(req.body);
+  console.log(result);
+  res.json(result);
 });
 
 app.listen(port, () => {
