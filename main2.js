5a20230c6350663e8d20a7ad9b302e952245b5d9
yosukekato165 <padyk165@gmail.com>
2021-12-18 16:45:35 +0900

    response json

--- main2.js
+++ main2.js
@@ -5,12 +5,17 @@ const port = 3030;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const hey = [
+  { name: "Tom", age: "18" },
+  { name: "John", age: "20" },
+];
+
 app.post("/hello", (req, res) => {
   res.send("Hello " + req.body.name);
 });
 
 app.post("/hey", (req, res) => {
-  res.send("Hey" + req.body.name);
+  res.json(hey);
 });
 
 app.listen(port, () => {
